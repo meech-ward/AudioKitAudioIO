fc48f20c6c00d3b98fa70e30b086fe2db548e640
Sam Meech-Ward <sam@meech-ward.com>
2018-01-12 08:33:57 -0800

    Update carthage files

--- scripts/build-carthage.sh
+++ scripts/build-carthage.sh
@@ -1,10 +1,16 @@
+AUDIO_KIT_GITHUB="https://github.com/AudioKit/AudioKit.git"
+AUDIO_KIT_BRANCH="develop"
+
 AUDIO_IO_DIR="$(pwd)/../../../Entities/AudioIO"
 
-echo "git \"$AUDIO_IO_DIR\"" > ../Cartfile
+echo "" > ../Cartfile
+echo "git \"$AUDIO_IO_DIR\"" >> ../Cartfile
+echo "git \"$AUDIO_KIT_GITHUB\" \"$AUDIO_KIT_BRANCH\"" >> ../Cartfile
 
 cd ../
 carthage update --platform iOS
 
+# Just for the swift package entities
 cd Carthage/Checkouts/AudioIO
 swift package generate-xcodeproj
 
