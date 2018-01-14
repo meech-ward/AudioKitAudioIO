08ab2d27e560d656aa97d057794f79ba0eaa5885
Sam Meech-Ward <sam@meech-ward.com>
2018-01-14 05:26:03 -0800

    Create an example project that uses the microhpone

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -7,12 +7,24 @@
 //
 
 import UIKit
+import AudioKitAudioIO
+import AudioIO
 
 class ViewController: UIViewController {
 
+  var audioInput: AudioIO.AudioInput!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    
+    audioInput = AudioIO.AudioInput(microphone: AudioKitAudioIO.Microphone(), bufferClosure: bufferUpdated, amplitudeTracker: AudioKitAudioIO.AmplitudeTracker())
+    audioInput.start()
+  }
+  
+  func bufferUpdated(_ buffer: UnsafeMutablePointer<Float>, _ bufferSize: Int) {
+    print("Buffer \(bufferSize)")
+    print("amplitude \(audioInput.amplitude ?? 0)")
   }
 
   override func didReceiveMemoryWarning() {
